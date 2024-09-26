408f82e8071e0fa4969cb0ef0c0d9f8f9145d761
Christian Williams <williechri79@gmail.com>
2024-09-26 18:59:05 -0400

    updated site - fixed emojis in links

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -38,11 +38,11 @@ const Experience = () => {
                 <p className="date">2021 - 2024</p>
                 <div>
                     <p className="job-desc">
-                        Developed, tested, and maintained the media app on the <a id="ps-link" href="https://www.playstation.com/en-us/ps4/">PS4↗</a> and <a id="ps-link" href="https://www.playstation.com/en-us/ps5/">PS5↗</a>. 
+                        Developed, tested, and maintained the media app on the <a id="ps-link" href="https://www.playstation.com/en-us/ps4/">PS4↗&#xFE0E;</a> and <a id="ps-link" href="https://www.playstation.com/en-us/ps5/">PS5↗&#xFE0E;</a>. 
                         Worked closely with cross-functional teams, including other developers, designers, and project managers to impliment accessibility compliant and pixel perfect frontend features and backend features.
                     </p>
-                    <a id="app-link" href="https://www.playstation.com/en-us/ps4/ps4-entertainment/">TV and Video app (PS4)↗</a>
-                    <a id="app-link" href="https://www.playstation.com/en-us/ps5/ps5-entertainment/">Media Hub (PS5)↗</a>
+                    <a id="app-link" href="https://www.playstation.com/en-us/ps4/ps4-entertainment/">TV and Video app (PS4)↗&#xFE0E;</a>
+                    <a id="app-link" href="https://www.playstation.com/en-us/ps5/ps5-entertainment/">Media Hub (PS5)↗&#xFE0E;</a>
                 </div>
                 <div className="tech-list">
                     <p>Javascript</p> <p>React</p> <p>Ember Js</p> <p>Java</p> <p>Spring Boot</p> <p>Node</p> <p>Jira</p> <p>Confluence</p> <p>AWS</p> <p>Chai</p> <p>Jest</p> <p>Mocha</p> <p>Junit</p>
